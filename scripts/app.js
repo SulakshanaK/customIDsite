7f0b88197bfea085162d5e5182b517de23fafab8
sulakshana <sulakshana.kodangal@estuate.com>
2016-07-18 12:48:02 +0530

    version 23

--- scripts/app.js
+++ scripts/app.js
@@ -265,7 +265,7 @@
       if(a.status == 400){
         console.log("400");
        //a.sent = !1, a.fields.email.setError("duplicateUser", !0)
-       a.sent = !1, a.submitting = !1, a && (400 === a.status ? a.fields.email.setError("unknownError", !0) : a.unknownError = String(a.userMessage || a.developerMessage || a))
+       a.sent = !1, a.submitting = !1 //, a && (400 === a.status ? a.fields.email.setError("unknownError", !0) : a.unknownError = String(a.userMessage || a.developerMessage || a))
        //a.status = "failed", a.error = String(b.userMessage || b.developerMessage || b.message || b)
 
       }
